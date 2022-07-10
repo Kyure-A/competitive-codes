3d34b797d3c3cb04f7f226be434dd7d2ea416129
root <root@ThinkCentre>
2022-07-11 00:04:15 +0900

    AC

--- typical90/★2/055.cpp
+++ typical90/★2/055.cpp
@@ -33,7 +33,7 @@ using namespace std;
 #define per3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
 
 /* Type */
-// #define int long long
+#define int long long
 #define cauto const auto&
 typedef long long int64;
 typedef long double ld;
@@ -50,8 +50,32 @@ signed main ()
 {
   int n, p, q;
   cin >> n >> p >> q;
+
+  vector<int> a(n);
   
+  rep(i,n) cin >> a[i];
+
+  int count = 0;
   
+  for (int i = 0; i < n - 4; ++i)
+    {
+      for (int j = i + 1; j < n - 3; ++j)
+	{
+	  for (int k = j + 1; k < n - 2; ++k)
+	    {
+	      for (int l = k + 1; l < n - 1; ++l)
+		{
+		  for (int m = l + 1; m < n; ++m)
+		    {
+		      if (a[i] % p * a[j] % p * a[k] % p * a[l] % p * a[m] % p == q) count++;
+		    }
+		  
+		}
+	    } 
+	}
+    }
+
+  cout << count << endl;
   
   return 0;
 }
